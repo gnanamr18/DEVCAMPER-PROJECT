21c1cfdbf059747afc17dc1a5dbad1b4bfb66a90
gnanamr18 <gnanam.r18@gmail.com>
2024-03-27 12:22:36 +0530

    imported data in db

--- seeder.js
+++ seeder.js
@@ -4,21 +4,16 @@ const colors = require("colors");
 const dotenv = require("dotenv");
 
 // Load env vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config();
 
 // Load models
 const Bootcamp = require("./models/Bootcamp");
-const Course = require("./models/Course");
-const User = require("./models/User");
-const Review = require("./models/Review");
+// const Course = require("./models/Course");
+// const User = require("./models/User");
+// const Review = require("./models/Review");
 
 // Connect to DB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(
@@ -41,9 +36,9 @@ const reviews = JSON.parse(
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
-    await Course.create(courses);
-    await User.create(users);
-    await Review.create(reviews);
+    // await Course.create(courses);
+    // await User.create(users);
+    // await Review.create(reviews);
     console.log("Data Imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -55,9 +50,9 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
-    await Course.deleteMany();
-    await User.deleteMany();
-    await Review.deleteMany();
+    // await Course.deleteMany();
+    // await User.deleteMany();
+    // await Review.deleteMany();
     console.log("Data Destroyed...".red.inverse);
     process.exit();
   } catch (err) {
--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,9 +1,9 @@
 const NodeGeocoder = require("node-geocoder");
 
 const options = {
-  provider: process.env.GEOCODER_PROVIDER,
+  provider: "mapquest",
   httpAdapter: "https",
-  apiKey: process.env.GEOCODER_API_KEY,
+  apiKey: "nOGhneiMId8iflFYE4CllLrWVIxghcxH",
   formatter: null,
 };
 
